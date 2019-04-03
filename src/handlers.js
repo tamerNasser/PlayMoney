e4db29a4c362b4de345577375deb4a1c97d8fc19
Tamer Nasser <tamernasser@outlook.com>
2019-04-03 20:26:33 +0300

    Made handler for server error and public

--- src/handlers.js
+++ src/handlers.js
@@ -13,6 +13,7 @@ const handleServer500 = (res,err)=>{
   res.writeHead(500,exType.html)
   res.end("<h1>Sorry, there was an error in the server.</h1><br/><h2>ERROR CODE: 500</h2><br/>Error details: <br/>"+err)
 }
+
 const handlePublic = (response, url) => {
   let filePath = path.join(__dirname, "..", url)
   const extension = url.split('.')[1]
