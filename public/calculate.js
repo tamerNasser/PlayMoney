e13cb032882e7a54899d6b02728548259c83ac22
Tamer Nasser <tamernasser@outlook.com>
2019-04-04 12:02:04 +0300

    Added functionality to submit button related #50 related #44 related #22

--- public/calculate.js
+++ public/calculate.js
@@ -1,21 +1,29 @@
-const cashInput = document.getElementById('currencyInput').value;
-const fromButton = document.getElementById('currencyFrom').options[fromButton.selectedIndex].value;
-const toButton = document.getElementById('currencyTo').options[toButton.selectedIndex].value;
+
+const resultsTxt = document.getElementById('results');
 
 
-fetch('/convertor?from=EUR&to=GBP')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    return(data);
-  })
-  .catch(function(error) {
-    return(error);
-  })
+function calculate(frombtn,tobtn,cashInput){
+function getRate(from,to,cb){
+  fetch('/convertor?from='+from+'&to='+to,cb)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      console.log("data",data);
+      console.log("from",from);
+      console.log("to",to);
+      return(cb(data));
+    })
+    .catch(function(error) {
+      return(error);
+    })
+}
 
-  submitButton.addEventListener('submit', function(e) {
-    console.log("button")
-    convertCurrency(fromCash, fromCurrency, toCurrency);
-    event.preventDefault();
-  }, false);
+getRate(frombtn, tobtn,function (d){
+  console.log("rate",parseFloat(d));
+  resultsTxt.value = (cashInput * parseFloat(d)).toFixed(2);
+});
+
+
+
+}
